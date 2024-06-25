cd482474cb2738988e43777dea11b109c0c8789d
MoralessJ <3647693780101@ingenieria.usac.edu.gt>
2024-06-24 21:45:47 -0600

    tasks hidden

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,20 @@ addEventListener("DOMContentLoaded", () => {
     Tasks();
 })
 
+const tailFormNormal = "bg-slate-600 border-white border-2 rounded-md px-3 py-1 text-white w-full";
+
 function Tasks(){
-    const add = document.querySelector(".btn-task");
+    const 
+        add = document.querySelector(".btn-task"),
+        overlay = document.querySelector(".overlay"),
+        taskForm = document.querySelector(".add-task");
+
     add.addEventListener("click", ()=> {
-        const overlay = document.querySelector(".overlay");
         overlay.classList.remove("hidden");
+        taskForm.classList.remove("hidden");
+    })
+    overlay.addEventListener("click", ()=>{
+        overlay.classList.add("hidden");
+        taskForm.classList.add("hidden");
     })
 }   
