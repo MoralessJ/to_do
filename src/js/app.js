e68fab772f3a3da0ca3fa7a33af3328e9b34251d
MoralessJ <3647693780101@ingenieria.usac.edu.gt>
2024-06-24 22:53:39 -0600

    Select text area

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
 addEventListener("DOMContentLoaded", () => {
-    Tasks();
+    showMenu();
+
 })
 
-const tailFormNormal = "bg-slate-600 border-white border-2 rounded-md px-3 py-1 text-white w-full";
+const 
+    tailFormNormal = "bg-slate-600 border-white border-2 rounded-md px-3 py-1 text-white w-full",
+    tailFormSelected = "bg-slate-600 border-white border-2 rounded-md px-3 pt-3 pb-1 text-white w-full";
 
-function Tasks(){
+function showMenu(){
     const 
         add = document.querySelector(".btn-task"),
         overlay = document.querySelector(".overlay"),
@@ -19,3 +22,33 @@ function Tasks(){
         taskForm.classList.add("hidden");
     })
 }   
+
+function addTask() {
+    const 
+        formNameTask = document.querySelector("#nameTask"),
+        textNameTask = document.querySelector(".labelNameTask"),
+        sectionTask = document.querySelector(".add-task");
+
+    let selected = false;
+
+    formNameTask.addEventListener("click", () => {
+        if (!selected)
+        {
+            formNameTask.className = tailFormSelected;
+            formNameTask.placeholder = "";
+            textNameTask.classList.remove("hidden");
+            selected = true;
+            console.log("seleccionado");
+        }
+    })
+    sectionTask.addEventListener("click", ()=> {
+        if (selected)
+        {
+            formNameTask.className = tailFormNormal;
+            formNameTask.placeholder = "Nombre de tu tarea";
+            textNameTask.classList.add("hidden");
+            selected = false;
+            console.log("ocultado");
+        }
+    })
+}
